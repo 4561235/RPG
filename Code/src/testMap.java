8de3f08caef974be0986cdea83c4b991d55d3375
Mateusz <szczenalol@gmail.com>
2018-05-10 22:58:28 +0200

    Generateur de Map v.1.0

---
+++ Code/src/testMap.java
@@ -0,0 +1,35 @@
+
+public class testMap {
+
+	public static void main(String[] args) {
+		
+		Map maMap = new Map(10,10);
+		System.out.println("longueur de la map:" +maMap.getLongueur());
+		System.out.println("largeur de la map:" +maMap.getLargeur());
+		
+		for(int x=0;x<10;x++)
+		{
+			maMap.mettreSurLaMap(x, 10, '#');
+		}
+		
+		for(int x=0;x<10;x++)
+		{
+			maMap.mettreSurLaMap(x, 0, '#');
+		}
+		
+		for(int y=1;y<9;y++)
+		{
+			maMap.mettreSurLaMap(0, y, '#');
+			for(int x=1;x<9;x++)
+			{
+				maMap.mettreSurLaMap(x, y, ' ');
+			}
+			maMap.mettreSurLaMap(10, y, '#');
+		}
+		
+
+		maMap.dessinerMap();
+	
+	}
+
+}
